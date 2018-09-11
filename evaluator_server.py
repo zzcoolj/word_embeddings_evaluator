a3e7228cc0081f2ed6fd44b598902a0949f44317
zzcoolj <zheng.zhang@limsi.fr>
2018-09-11 15:00:28 +0200

    ppmi_svd evaluate ready

--- evaluator_server.py
+++ evaluator_server.py
@@ -8,6 +8,6 @@ for w in range(2, 11):
     e = evaluator.Evaluator.from_storage(
         tokens_path='../matrix2vec/input/encoded_edges_count_window_size_' + str(w) + '_undirected_tokens.pickle')
     for d in [200, 500, 800, 1000]:
-        result = e.evaluate('ppmi_svd_w' + str(w) + '_d' + str(d) + '.npy', matrix_type='npy')
+        result = e.evaluate('../matrix2vec/output/vectors/ppmi_svd/ppmi_svd_w' + str(w) + '_d' + str(d) + '.npy', matrix_type='npy')
         print(result)
         exit()
