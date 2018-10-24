49ecd9936ac392d03d3a3308f6d039bae9403315
Zheng ZHANG <zzcoolj@client-172-18-74-95.clients.u-psud.fr>
2018-10-24 16:19:47 +0200

    super concatenate

--- evaluator_server.py
+++ evaluator_server.py
@@ -65,7 +65,7 @@ Evaluate results in ppmi+rw1_svd
 """
 
 
-def evaluate_folder_bis(folder_path, window_size, file_name_prefix):
+def evaluate_folder_elementWiseMerge(folder_path, window_size, file_name_prefix):
     df = pd.DataFrame(columns=[
                 # word embeddings file name
                 'file name',
@@ -102,22 +102,56 @@ def evaluate_folder_bis(folder_path, window_size, file_name_prefix):
         os.remove(file_path)
 
 
-# evaluate_folder_bis(folder_path='../matrix2vec/output/vectors/ppmi+firstOrder_svd/', window_size=5, file_name_prefix='ppmi_w5_+firstOrder_w5_k')
-# evaluate_folder_bis(folder_path='../matrix2vec/output/vectors/ppmi+rw1_svd/', window_size=5, file_name_prefix='ppmi_w5_+rw1_w7_k')
-# evaluate_folder_bis(folder_path='../matrix2vec/output/vectors/ppmi+rw2_svd/', window_size=5, file_name_prefix='ppmi_w5_+rw2_w3_k')
-# evaluate_folder_bis(folder_path='../matrix2vec/output/vectors/cooc_firstOrder_normalized_svd/', window_size=5, file_name_prefix='cooc_w5_firstOrder_w5_normalized_k')
+# evaluate_folder_elementWiseMerge(folder_path='../matrix2vec/output/vectors/ppmi+firstOrder_svd/', window_size=5, file_name_prefix='ppmi_w5_+firstOrder_w5_k')
+# evaluate_folder_elementWiseMerge(folder_path='../matrix2vec/output/vectors/ppmi+rw1_svd/', window_size=5, file_name_prefix='ppmi_w5_+rw1_w7_k')
+
 
 '''
-specific
+Evaluate single
 '''
 
-e = evaluator.Evaluator.from_storage(tokens_path='../matrix2vec/input/encoded_edges_count_window_size_5_undirected_tokens.pickle')
+
+# e = evaluator.Evaluator.from_storage(tokens_path='../matrix2vec/input/encoded_edges_count_window_size_5_undirected_tokens.pickle')
 # print(e.evaluate(matrix_path='../matrix2vec/output/vectors/specific/specific_k-1_svd_d500.npy', matrix_type='npy'))
 # print(e.evaluate(matrix_path='../matrix2vec/output/vectors/specific/specific_k-1_svd_d800.npy', matrix_type='npy'))
 # print(e.evaluate(matrix_path='../matrix2vec/output/vectors/specific/specific_k-1_svd_d1000.npy', matrix_type='npy'))
 
-print(e.evaluate(matrix_path='../matrix2vec/output/vectors/specific/m4_300.npy', matrix_type='npy'))
-print(e.evaluate(matrix_path='../matrix2vec/output/vectors/specific/m4_500.npy', matrix_type='npy'))
-print(e.evaluate(matrix_path='../matrix2vec/output/vectors/specific/m4_700.npy', matrix_type='npy'))
-print(e.evaluate(matrix_path='../matrix2vec/output/vectors/specific/m4_900.npy', matrix_type='npy'))
-print(e.evaluate(matrix_path='../matrix2vec/output/vectors/specific/m4_1000.npy', matrix_type='npy'))
+
+"""
+
+"""
+
+
+def evaluate_folder_superConcatenate(files_prefix, base_window_size, dimensions, window_size_max, output_folder_path):
+    df = pd.DataFrame(columns=[
+        # word embeddings file name
+        'file name',
+        # wordsim353
+        'wordsim353_Pearson correlation', 'Pearson pvalue',
+        'Spearman correlation', 'Spearman pvalue', 'Ration of pairs with OOV',
+        # simlex999
+        'simlex999_Pearson correlation', 'Pearson pvalue',
+        'Spearman correlation', 'Spearman pvalue', 'Ration of pairs with OOV',
+        # questions-words
+        'sem_acc', '#sem', 'syn_acc', '#syn', 'total_acc', '#total'
+    ])
+    i = 0
+
+    for w in range(2, window_size_max+1):
+        e = evaluator.Evaluator.from_storage(
+            tokens_path='../matrix2vec/input/encoded_edges_count_window_size_' + str(base_window_size) +
+                        '_undirected_tokens.pickle')
+        for d in dimensions:
+            file_path = files_prefix + str(w) + '_d' + str(d)
+            result = e.evaluate(file_path + '.npy', matrix_type='npy')
+            df.loc[i] = [file_path] + result
+            print([file_path] + result)
+            i += 1
+
+            # delete relevant files
+            os.remove(file_path + '.npy')
+            os.remove(file_path + '_embeddings.csv')
+
+    writer = pd.ExcelWriter(output_folder_path + 'result'+str(base_window_size)+'.xlsx')
+    df.to_excel(writer, 'Sheet1')
+    writer.save()
